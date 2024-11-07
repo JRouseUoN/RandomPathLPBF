9906736483a74e5af04a40712a6a2797e4df29bb
JRouseUoN <james.rouse@nottingham.ac.uk>
2024-11-07 15:05:38 +0000

    Working code. Plots now shown between correct limits.

--- 2dMoving.py
+++ 2dMoving.py
@@ -18,9 +18,9 @@ def gaussian(x, y, mu_x, mu_y, sigma_x, sigma_y):
 
 # Parameters
 Lx, Ly = 100, 100  # Length of the plane in x and y directions
-T = 1.0   # Total time
+T = 2.0   # Total time
 Nx, Ny = 500, 500  # Number of spatial points in x and y directions
-Nt = 10  # Number of time points
+Nt = 80  # Number of time points
 
 density=7.8e-6 # kg/mm^3
 thermal_conductivity=45e-3 # W/mmK
@@ -35,31 +35,28 @@ dt = T / (Nt - 1)
 thickness=1 #plate thickness 
 
 # Parameters for the Gaussian
-mu_x = 50
-mu_y = 50
-sigma_x = 1
-sigma_y = 1
+mu_x = 30
+mu_y = 30
+sigma_x = 1.5
+sigma_y = 1.5
 
 # Moving heat source parameters
-source_amplitude = 0.001
+source_amplitude = 0.1
 # source_width = 0.1
 source_speed = 20
 
+
+
 # Coordinate Matrices
-x_mat=np.ones((Nx,Ny))
-for i in range(0,Nx,1):
-    x_mat[:,i]=x_mat[:,i]*(i*dx)
+x_mat, y_mat = np.meshgrid(np.linspace(0, Lx, Nx), np.linspace(0, Ly, Ny))
 
-
-y_mat=np.ones((Nx,Ny))
-for i in range(0,Ny,1):
-    y_mat[i,:]=y_mat[i,:]*(i*dy)
 
 # Initial temperature distribution
 u = np.zeros((Nt, Nx, Ny))
 heat_source = np.zeros((Nt, Nx, Ny))
 # Set Boundary Condition and initial state
-u[0,:,:]=np.ones((Nx, Ny))*20
+initial_temperature=20
+u[0,:,:]=np.ones((Nx, Ny))*initial_temperature
 
    
 # Starting heat source            
@@ -89,42 +86,40 @@ for n in range(1, Nt,1):
     deltaT=A.dot(b)
     T_new_flattened=b+(deltaT*dt)
     u[n,:,:]=T_new_flattened.reshape((Nx, Ny))
-    
-    
-    
     # Evaluate heat source
-    # heat_source_temp = gaussian(x_mat, y_mat, mu_x, mu_y, sigma_x, sigma_y)    
-    # heat_source_temp = heat_source_temp*source_amplitude
-    # heat_source[n,:,:] = heat_source_temp
-    # # Apply heat source to temperature field
-    # u_temp=heat_source_temp*(1/(dx*dy*thickness*density))*(1/specific_heat)
-    # u[n, :, :]=u[n, :, :]+u_temp
-    # # Update heat source location
-    # mu_y=mu_y+(source_speed*dt)
-    # print(n)
+    heat_source_temp = gaussian(x_mat, y_mat, mu_x, mu_y, sigma_x, sigma_y)    
+    heat_source_temp = heat_source_temp*source_amplitude
+    heat_source[n,:,:] = heat_source_temp
+    # Apply heat source to temperature field
+    u_temp=heat_source_temp*(1/(dx*dy*thickness*density))*(1/specific_heat)
+    u[n, :, :]=u[n, :, :]+u_temp
+    #Ensure outer edge is constant temperature
+    u[n, 0, :]=initial_temperature
+    u[n, Nx-1, :]=initial_temperature
+    u[n, :, 0]=initial_temperature
+    u[n, :, Ny-1]=initial_temperature
+    # Update heat source location
+    mu_x=mu_x+(source_speed*dt)
+    mu_y=mu_y+(source_speed*dt)
+    print(n)
 
 
-
-
+vmin=0
+vmax=800
+colorbar_ticks = np.round(np.linspace(vmin, vmax, 6))
 # # Plot the results
 plt.figure(figsize=(10, 6))
-X, Y = np.meshgrid(np.linspace(0, Lx, Nx), np.linspace(0, Ly, Ny))
-for n in range(0, Nt, 1):
-    plt.contourf(X, Y, u[n].T, levels=50, cmap='hot')
-    plt.colorbar()
+for n in range(0, Nt, 10):
+    plt.contourf(x_mat, y_mat, u[n,:,:], cmap='hot', levels=np.linspace(vmin, vmax, 50), vmin=vmin, vmax=vmax)
+    cbar = plt.colorbar(label='Temperature (deg C)', ticks=colorbar_ticks)
+    cbar.ax.set_yticklabels([str(tick) for tick in colorbar_ticks])
     plt.title(f'Temperature distribution at t={n*dt:.2f}s')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
 
 
-# # Plot the results
-# plt.figure(figsize=(10, 6))
-# X, Y = np.meshgrid(np.linspace(0, Lx, Nx), np.linspace(0, Ly, Ny))
-# for n in range(0, Nt, 1):
-#     plt.contourf(X, Y, heat_source[n].T, levels=50, cmap='hot')
-#     plt.colorbar()
-#     plt.title(f'Heat Source at t={n*dt:.2f}s')
-#     plt.xlabel('x')
-#     plt.ylabel('y')
-#     plt.show()
+
+
+
+
